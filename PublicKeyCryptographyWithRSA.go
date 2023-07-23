30cee118e47dfa8b0ed1dc9bf2b2eb0f8ef3e1c9
Frederick Hieronymus <frederick@disroot.org>
2023-07-23 23:40:27 +0200

    Step 6: Test for Primality

--- PublicKeyCryptographyWithRSA.go
+++ PublicKeyCryptographyWithRSA.go
@@ -2,140 +2,115 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
 	"time"
 )
 
-var primes []int
+// Initialize a pseudorandom number generator.
+var random = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+const numTests = 20
 
 func main() {
-	primes = sieveToPrimes(eulersSieve(20_000_000))
+	testKnownValues()
+
+	fmt.Println()
 
 	for {
-		var num int
+		var digits int
 
-		fmt.Print("Enter num : ")
-		if _, err := fmt.Scanln(&num); err != nil {
+		fmt.Print("Enter digits : ")
+		if _, err := fmt.Scanln(&digits); err != nil {
 			fmt.Println("Error: ", err)
 		}
 
-		if num < 2 {
+		if digits < 1 {
 			break
 		}
 
-		// Find the factors the slow way.
-		start := time.Now()
-		factors := findFactors(num)
-		elapsed := time.Since(start)
-		fmt.Printf("findFactors: %f seconds\n", elapsed.Seconds())
-		fmt.Printf("Prime factors of %d are : %v\n", num, factors)
-		fmt.Println("Multiplication of prime factors =", multiplySlice(factors))
+		min := int64(math.Pow(float64(10), float64(digits-1)))
+		max := min * 10
+
+		if min == 1 {
+			min = 2
+		}
+		fmt.Printf("Prime: %d\n\n", findPrime(min, max, numTests))
+	}
+}
+
+// Return a pseudo random number in the range [min, max).
+func randRange(min, max int64) int64 {
+	return min + random.Int63n(max-min)
+}
+
+func isProbablyPrime(p int64, numTests int) bool {
+	for i := 0; i < numTests; i++ {
+		n := randRange(2, p)
+		result := fastExpMod(n, p-1, p)
+		if result != 1 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func fastExpMod(num, pow, mod int64) int64 {
+	var result int64 = 1
+	for pow > 0 {
+		if pow%2 == 1 {
+			result = (result * num) % mod
+		}
+		pow /= 2
+		num = (num * num) % mod
+	}
+	return result
+}
+
+func findPrime(min, max int64, numTests int) int64 {
+	for {
+		n := randRange(min, max+1)
+		if n%2 == 0 {
+			continue
+		}
+
+		if isProbablyPrime(n, numTests) {
+			return n
+		}
+	}
+}
+
+func testKnownValues() {
+	primes := []int{
+		10009, 11113, 11699, 12809, 14149,
+		15643, 17107, 17881, 19301, 19793,
+	}
+	composites := []int{
+		10323, 11397, 12212, 13503, 14599,
+		16113, 17547, 17549, 18893, 19999,
+	}
+
+	fmt.Printf("Probability: %f%%\n", (1-1/math.Pow(float64(2), float64(numTests)))*100)
+	fmt.Println()
+
+	fmt.Println("Primes:")
+	for _, prime := range primes {
+		fmt.Printf("%d", prime)
+		if isProbablyPrime(int64(prime), numTests) {
+			fmt.Printf("\tPrime")
+		}
 		fmt.Println()
+	}
 
-		// Use the Euler's sieve to find the factors.
-		start = time.Now()
-		factors = findFactorsSieve(num)
-		elapsed = time.Since(start)
-		fmt.Printf("findFactorsSieve: %f seconds\n", elapsed.Seconds())
-		fmt.Printf("Prime factors of %d are : %v\n", num, factors)
-		fmt.Println("Multiplication of prime factors =", multiplySlice(factors))
+	fmt.Println()
+
+	fmt.Println("Composites:")
+	for _, composite := range composites {
+		fmt.Printf("%d", composite)
+		if !isProbablyPrime(int64(composite), numTests) {
+			fmt.Printf("\tComposite")
+		}
 		fmt.Println()
 	}
-}
-
-func findFactors(num int) []int {
-	var primeFactors []int
-
-	for num%2 == 0 {
-		primeFactors = append(primeFactors, 2)
-		num /= 2
-	}
-
-	for i := 3; i*i <= num; i = i + 2 {
-		for num%i == 0 {
-			primeFactors = append(primeFactors, i)
-			num /= i
-		}
-	}
-
-	if num > 2 {
-		primeFactors = append(primeFactors, num)
-	}
-
-	return primeFactors
-}
-
-func multiplySlice(primeFactors []int) int {
-	num := 1
-	for _, factor := range primeFactors {
-		num *= factor
-	}
-	return num
-}
-
-func sieveToPrimes(sieve []bool) []int {
-	var primesSlice []int
-
-	if len(sieve) > 2 {
-		primesSlice = append(primesSlice, 2)
-	}
-	for i := 3; i < len(sieve); i += 2 {
-		if sieve[i] {
-			primesSlice = append(primesSlice, i)
-		}
-	}
-
-	return primesSlice
-}
-
-// Build an Euler’s Sieve.
-func eulersSieve(max int) []bool {
-	prime := make([]bool, max+1)
-
-	if max < 2 {
-		return prime
-	}
-
-	prime[2] = true
-	for i := 3; i <= max; i += 2 {
-		prime[i] = true
-	}
-
-	for i := 3; i*i <= max; i += 2 {
-		if prime[i] {
-			maxQuotient := max / i
-			if maxQuotient%2 == 0 {
-				maxQuotient--
-			}
-			for j := maxQuotient; j >= i; j -= 2 {
-				if prime[j] {
-					prime[i*j] = false
-				}
-			}
-		}
-	}
-
-	return prime
-}
-
-func findFactorsSieve(num int) []int {
-	var primeFactors []int
-
-	for num%2 == 0 {
-		primeFactors = append(primeFactors, 2)
-		num /= 2
-	}
-
-	for i := 1; i < len(primes); i++ {
-		prime := primes[i]
-		for num%prime == 0 {
-			primeFactors = append(primeFactors, prime)
-			num /= prime
-		}
-	}
-
-	if num > 2 {
-		primeFactors = append(primeFactors, num)
-	}
-
-	return primeFactors
 }
